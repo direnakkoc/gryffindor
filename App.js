3eecb94f8599309e238370bae76fbd45e0f97e2f
Walter Guimarães Junior <walterguimaraesjr@gmail.com>
2021-03-23 21:19:57 +0000

    Creating the Project - TEST

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome!!! This is our Project</Text>
       <StatusBar style="auto" />
     </View>
   );
