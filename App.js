bc75ef0fa7dfd8002ef15f95838cb4e086d09580
direnakkocdemir <drnakkoc94@gmail.com>
2021-04-06 13:31:57 +0100

    Diren-06/04

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
-//
 import React, { useState, useEffect, useRef } from "react";
 import { View, TouchableOpacity, Modal, Image } from "react-native";
 import { Camera } from "expo-camera";
 import Icon from "react-native-vector-icons/FontAwesome";
-import * as Permissions from 'expo-permissions';
-import * as MediaLibrary from 'expo-media-library';
+import * as Permissions from "expo-permissions";
+import * as MediaLibrary from "expo-media-library";
+
+/*expo install expo-camera
+ expo install expo-media-library*/ 
 
 export default function App() {
-
   const [hasPermission, setHasPermission] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
   const camRef = useRef(null);
@@ -15,13 +16,17 @@ export default function App() {
   const [open, setOpen] = useState(false);
 
   useEffect(() => {
-    { /* request to access camera */ }
+    {
+      /* request to access camera */
+    }
     (async () => {
       const { status } = await Camera.requestPermissionsAsync();
       setHasPermission(status === "granted");
     })();
 
-    { /* request access media library */ }
+    {
+      /* request access media library */
+    }
     (async () => {
       const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
       setHasPermission(status === "granted");
@@ -53,20 +58,16 @@ export default function App() {
   async function savePicture() {
     const asset = await MediaLibrary.createAssetAsync(takenPhoto)
       .then(() => {
-        alert("Photo succesfully saved!")
+        alert("Photo succesfully saved!");
       })
-      .catch(error => {
-        console.log('err', error);
-      })
+      .catch((error) => {
+        console.log("err", error);
+      });
   }
 
   return (
     <View style={{ flex: 1 }}>
-      <Camera
-        style={{ flex: 3 }}
-        type={type}
-        ref={camRef}>
-      </Camera>
+      <Camera style={{ flex: 3 }} type={type} ref={camRef}></Camera>
       <View
         style={{
           flex: 1,
@@ -83,6 +84,8 @@ export default function App() {
           <Icon name="camera" size={70} color="white" onPress={takePicture} />
         </View>
         <View style={{ flex: 1, alignItems: "center" }}>
+          
+          {/*code to demonstrate switching camera icon*/}
           <TouchableOpacity
             style={{
               borderWidth: 1,
@@ -93,6 +96,7 @@ export default function App() {
               alignItems: "center",
               justifyContent: "center",
             }}
+            /* function for switching camera from front to back and back to front*/
             onPress={() => {
               setType(
                 type === Camera.Constants.Type.back
@@ -105,32 +109,40 @@ export default function App() {
           </TouchableOpacity>
 
           {/* code to show the photo just taken and save it;
-          got from https://www.youtube.com/watch?v=h8ukVeuzHEY */ }
-          {takenPhoto &&
-            <Modal
-              animationType='slide'
-              transparent={false}
-              visible={open}
-            >
-              <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', margin: 20 }}>
-
-                <View style={{ margin: 10, flexDirection: 'row' }}>
-                  <TouchableOpacity style={{ margin: 10 }} onPress={() => setOpen(false)}>
-                    <Icon name='window-close' size={40} color="#FF0000" />
+          got from https://www.youtube.com/watch?v=h8ukVeuzHEY */}
+          {takenPhoto && (
+            <Modal animationType="slide" transparent={false} visible={open}>
+              <View
+                style={{
+                  flex: 1,
+                  justifyContent: "center",
+                  alignItems: "center",
+                  margin: 20,
+                }}
+              >
+                <View style={{ margin: 10, flexDirection: "row" }}>
+                  <TouchableOpacity
+                    style={{ margin: 10 }}
+                    onPress={() => setOpen(false)}
+                  >
+                    <Icon name="window-close" size={40} color="#FF0000" />
                   </TouchableOpacity>
 
-                  <TouchableOpacity style={{ margin: 10 }} onPress={savePicture}  >
-                    <Icon name='upload' size={40} color="#121212" />
+                  <TouchableOpacity
+                    style={{ margin: 10 }}
+                    onPress={savePicture}
+                  >
+                    <Icon name="upload" size={40} color="#121212" />
                   </TouchableOpacity>
                 </View>
 
                 <Image
-                  style={{ width: '100 %', height: '100%' }}
-                  source={{ uri: takenPhoto }} />
-
+                  style={{ width: "100 %", height: "100%" }}
+                  source={{ uri: takenPhoto }}
+                />
               </View>
             </Modal>
-          }
+          )}
         </View>
       </View>
     </View>
