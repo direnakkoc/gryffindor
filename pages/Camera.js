8a4a608a294c9ccff06bd7edf6227c0658667882
Walter Guimarães Junior <walterguimaraesjr@gmail.com>
2021-04-08 12:46:00 +0100

    Updates on Camera code - 08/07/2021
    I copied the Gabriel's code to the interface and now it is taking photos.

--- pages/Camera.js
+++ pages/Camera.js
@@ -1,33 +1,64 @@
 import React, { useState, useEffect, useRef } from "react";
-import { View, TouchableOpacity, Text } from "react-native";
+import { View, TouchableOpacity, Modal, Image } from "react-native";
 import { Camera } from "expo-camera";
 import Icon from "react-native-vector-icons/FontAwesome";
+import * as Permissions from 'expo-permissions';
+import * as MediaLibrary from 'expo-media-library';
 
 export default function App() {
 
   const [hasPermission, setHasPermission] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
   const camRef = useRef(null);
+  const [takenPhoto, setTakenPhoto] = useState(null);
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
+    { /* request to access camera */ }
     (async () => {
       const { status } = await Camera.requestPermissionsAsync();
       setHasPermission(status === "granted");
     })();
+
+    { /* request access media library */ }
+    (async () => {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      setHasPermission(status === "granted");
+    })();
   }, []);
 
+  //Validating any permission
   if (hasPermission === null) {
     return <View />;
   }
   if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
+
+    return <Text>Access denied</Text>;
+    //I changed the text here so it can be a message for either permission
   }
 
   
+  /*function takePicture to take a picture
+    got from https://www.youtube.com/watch?v=h8ukVeuzHEY */
   async function takePicture() {
     if (camRef) {
       const data = await camRef.current.takePictureAsync();
+      setTakenPhoto(data.uri);
+      setOpen(true);
+      console.log(data);
     }
+  }
+
+  /*function takePicture to take a picture
+   got from https://www.youtube.com/watch?v=tj58H9eAJv0 */
+  async function savePicture() {
+    const asset = await MediaLibrary.createAssetAsync(takenPhoto)
+      .then(() => {
+        alert("Photo succesfully saved!")
+      })
+      .catch(error => {
+        console.log('err', error);
+      })
   }
 
   return (
@@ -50,7 +81,7 @@ export default function App() {
           <Icon name="square" size={50} color="white" />
         </View>
         <View style={{ flex: 1, alignItems: "center", padding: 50 }}>
-          <Icon name="camera" size={70} color="white" />
+          <Icon name="camera" size={70} color="white" onPress={takePicture} />
         </View>
         <View style={{ flex: 1, alignItems: "center" }}>
           <TouchableOpacity
@@ -73,9 +104,36 @@ export default function App() {
           >
             <Icon name="undo" size={30} color="black" />
           </TouchableOpacity>
+
+          {/* code to show the photo just taken and save it;
+          got from https://www.youtube.com/watch?v=h8ukVeuzHEY */ }
+          {takenPhoto &&
+            <Modal
+              animationType='slide'
+              transparent={false}
+              visible={open}
+            >
+              <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', margin: 20 }}>
+
+                <View style={{ margin: 10, flexDirection: 'row' }}>
+                  <TouchableOpacity style={{ margin: 10 }} onPress={() => setOpen(false)}>
+                    <Icon name='window-close' size={40} color="#FF0000" />
+                  </TouchableOpacity>
+
+                  <TouchableOpacity style={{ margin: 10 }} onPress={savePicture}  >
+                    <Icon name='upload' size={40} color="#121212" />
+                  </TouchableOpacity>
+                </View>
+
+                <Image
+                  style={{ width: '100 %', height: '100%' }}
+                  source={{ uri: takenPhoto }} />
+
+              </View>
+            </Modal>
+          }
         </View>
       </View>
     </View>
   );
 }
-
