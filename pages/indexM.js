c18725c02e4456642ad742df5b8c824769d8a683
Walter Guimarães Junior <walterguimaraesjr@gmail.com>
2021-04-18 21:59:43 +0100

    Updates in the Page about us

--- pages/indexM.js
+++ pages/indexM.js
@@ -4,12 +4,7 @@ PAGE FOR EDITING-- FREE PAGE */
 import React,{useEffect,useState} from 'react';
 import { View, Text, Image, StyleSheet, ImageBackground, ScrollView, requireNativeComponent } from 'react-native';
 import { Avatar } from 'react-native-paper';
-
 import * as Font from 'expo-font';
-
-
-
-
 
 export default function indexM() {
     const[fontsLoaded, setFontsLoaded] = useState(false);
@@ -21,58 +16,100 @@ export default function indexM() {
             });
         }
     });
+  
     return (
 
-        <ScrollView>
+       
         <View style={styles.container}>
-
-            <View style={{ paddingTop: 15, paddingHorizontal: 20 }}>
-
-                <Avatar.Image size={250} source={require('../assets/Diren.png')} />
-                <Text style={styles.text}>
-                    Diren Akkoc Demir - 2020266
-                            </Text>
-
-                <Avatar.Image size={250} source={require('../assets/Gabriel.png')} />
-                <Text style={styles.text}>
-                    Gabriel Climaco Brites Farina - 2020419
-                </Text>
-
-                <Avatar.Image size={250} source={require('../assets/Steffany.png')} />
-                <Text style={styles.text}>
-                    Steffany Aseret Roa Cañedo - 2020431
-                            </Text>
-
-                <Avatar.Image size={250} source={require('../assets/Walter.png')} />
-                <Text style={styles.text}>
-                    Walter Guimarães Junior - 2020403
-                    </Text>
-
-            </View>
+             <ScrollView>
+            <View style={styles.row} >
             
+                <View style={{ paddingTop: 15, paddingHorizontal: 20, width: '50%' }} >
+                    <Avatar.Image size={250} source={require('../assets/Diren.png')} />
+                        <Text style={styles.text}>
+                            Diren Akkoc Demir - 2020266
+                        </Text>
+
+                    <Avatar.Image size={250} source={require('../assets/Gabriel.png')} />
+                        <Text style={styles.text}>
+                            Gabriel Climaco Brites Farina - 2020419
+                        </Text>
+                </View>
+
+                <View style={{ paddingTop: 15, paddingHorizontal: 20, width: '50%' }} >
+                    <Avatar.Image size={250} source={require('../assets/Steffany.png')} />
+                        <Text style={styles.text}>
+                            Steffany Aseret Roa Cañedo - 2020431
+                        </Text>
+
+                    <Avatar.Image size={250} source={require('../assets/Walter.png')} />
+                        <Text style={styles.text}>
+                            Walter Guimarães Junior - 2020403
+                        </Text>
+                </View>
+                
+            </View> 
+            
+            <Image style={{width: 550, height: 150, borderRadius: 45 }}
+                source={require('../assets/cct_logo.png')}
+            />
+
+            <View>
+            <Text style={styles.quote}>
+                            "None of us is as smart as all of us".(Ken Blanchard)
+                        </Text>
+            </View> 
+            </ScrollView>
         </View>
-        </ScrollView>
+        
 
     )
-
 }
 
 const styles = StyleSheet.create({
 
     container: {
+        flex: 1,
+        flexDirection: 'row',
         justifyContent: "center",
-        backgroundColor: "#740001",
         alignItems: "center",
-        flexDirection: "column"
-
+        flexDirection: "column",
+        flex: 1,
+        backgroundColor: "#740001",
+        paddingTop: 15,
+        paddingHorizontal: 20,
+        
     },
     text: {
         color: "white",
-        //fontFamily:'Charlotte',
+        fontFamily:'Charlotte',
         fontWeight: 'bold',
-        fontSize: 15,
+        fontSize: 20,
         fontStyle: 'italic',
         textAlign: "center",
+       
         
-    }
+    },
+    quote: {
+        color: "#cb9232",
+        fontFamily:'Charlotte',
+        fontWeight: 'bold',
+        fontSize: 20,
+        fontStyle: 'italic',
+        textAlign: "center",
+       
+        
+    },
+    row: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 10,
+        //backgroundColor: "#740001"
+      },
+      logo: {
+        
+      }
+
+
 });
