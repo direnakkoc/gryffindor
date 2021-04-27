14981e9014e5f08ff9d326a5e5b13275b2381740
xexenias <sroa@Steffanys-MacBook-Air.local>
2021-04-27 19:03:27 +0100

    comments

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 
 //Page where is the link to the navigator.
-//Libraries generated
+//Libraries import to install the navigator
+// npm install navigator
 import React from 'react'
 import {NavigationContainer} from '@react-navigation/native';
 import Navigator from './Components/Navigator'
