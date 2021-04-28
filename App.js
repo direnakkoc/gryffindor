a07b9ad6b995ecb684082a8f175244320c7ddf54
xexenias <sroa@Steffanys-MacBook-Air.local>
2021-04-28 10:32:47 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 
 //STEFFY
 //App page where is the link to navigator
-//Necessary installation install react-navigation/native
+//Necessary installation react-navigation/native
 import React from 'react'
 import {NavigationContainer} from '@react-navigation/native';
 import Navigator from './Components/Navigator'
 export default function App(){
-//retun the navigation
+//return the navigation.js
   return (
     <NavigationContainer>
 <Navigator/>
